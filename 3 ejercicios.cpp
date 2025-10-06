0f439cc9c0fa4d099cf870f6c9176617fe2d9533
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-06 13:13:21 -0500

    Create 3 ejercicios.cpp

---
+++ 3 ejercicios.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5};
+    int suma = accumulate(v.begin(), v.end(), 0);
+    cout << "Suma total: " << suma;
+}
