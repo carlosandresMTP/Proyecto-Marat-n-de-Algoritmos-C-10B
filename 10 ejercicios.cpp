9d4966d3675479a321761ed206267e23ce2de3c1
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-06 13:19:24 -0500

    Create 10 ejercicios.cpp

---
+++ 10 ejercicios.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    priority_queue<int> pq;
+    pq.push(10);
+    pq.push(5);
+    pq.push(20);
+    while (!pq.empty()) {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+}
