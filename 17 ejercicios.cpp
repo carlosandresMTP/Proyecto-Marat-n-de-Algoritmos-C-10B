eb51f5e8d752be958e8be2f3aa932c110f6e73bb
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-12 18:10:58 -0500

    Create 17 ejercicios.cpp

---
+++ 17 ejercicios.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v = {4,2,5,1,3};
+    sort(v.begin(), v.end());
+    for(int x: v) cout<<x<<" ";
+    cout<<"\n";
+    return 0;
+}
