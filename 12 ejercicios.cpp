dcb94b06df171f4a7ae6091d90996aa2649980b9
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-12 18:07:43 -0500

    Create 12 ejercicios.cpp

---
+++ 12 ejercicios.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v(100);
+    iota(v.begin(), v.end(), 1);
+    for(int x: v) cout<<x<<" ";
+    cout<<"\n";
+    return 0;
+}
