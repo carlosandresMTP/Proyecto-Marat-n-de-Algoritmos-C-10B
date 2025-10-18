dbc1e5d91356bb594059c4decdd7ff0d548d4fc9
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-18 08:57:29 -0500

    Add function to display entered number

---
+++ 23 ejercicio.cpp
@@ -0,0 +1,12 @@
+// 102. Función que muestra un número ingresado
+#include <iostream>
+using namespace std;
+void mostrarNumero(int n) {
+    cout << "El numero es: " << n;
+}
+int main() {
+    int x;
+    cin >> x;
+    mostrarNumero(x);
+    return 0;
+}
