c1e21151b1ded33fd97e8d149400a5cc68c1e41a
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-18 09:10:01 -0500

    Implement function to find the smallest of three numbers

---
+++ 34 ejercicio.cpp
@@ -0,0 +1,15 @@
+// 113. Función que retorna el menor de tres números
+#include <iostream>
+using namespace std;
+int menor(int a, int b, int c) {
+    int m = a;
+    if (b < m) m = b;
+    if (c < m) m = c;
+    return m;
+}
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << "Menor: " << menor(x, y, z);
+    return 0;
+}
