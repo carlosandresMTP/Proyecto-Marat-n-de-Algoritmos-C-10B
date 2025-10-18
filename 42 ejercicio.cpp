261ca1681ff085a122ce8e56bc52626f82d669bc
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-18 09:15:23 -0500

    Add program to read and display 5 numbers
    Implement a program to read and display 5 numbers.

---
+++ 42 ejercicio.cpp
@@ -0,0 +1,9 @@
+// 81. Leer 5 números y mostrarlos
+#include <iostream>
+using namespace std;
+int main() {
+    int v[5];
+    for (int i = 0; i < 5; i++) cin >> v[i];
+    for (int i = 0; i < 5; i++) cout << v[i] << " ";
+    return 0;
+}
