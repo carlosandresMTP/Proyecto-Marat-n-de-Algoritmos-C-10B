97a364e9b82cdf501af3f5f68e3831983ded99f7
carlosandresMTP <cvillamar@alumno.montepiedra.edu.ec>
2025-10-18 08:56:49 -0500

    Add greeting function in 22ejercicio.cpp

---
+++ 22ejercicio.cpp
@@ -0,0 +1,10 @@
+// 101. Función que muestra un saludo
+#include <iostream>
+using namespace std;
+void saludar() {
+    cout << "Hola, bienvenido a C++!";
+}
+int main() {
+    saludar();
+    return 0;
+}
